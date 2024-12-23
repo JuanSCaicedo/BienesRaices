65bd7632a9f210d39039b54b40e1b5e4d2023377
JuanSCaicedo <sebanchit@gmail.com>
2024-12-23 04:42:31 +0000

    actualizando dependencias

--- includes/funciones.php
+++ includes/funciones.php
@@ -2,7 +2,7 @@
 
 define('TEMPLATES_URL', __DIR__ . '/templates');
 define('FUNCIONES_URL', __DIR__ . 'funciones.php');
-define('CARPETA_IMAGENES', $_SERVER['DOCUMENT_ROOT'] . '/imagenes/');
+define('CARPETA_IMAGENES', $_SERVER['DOCUMENT_ROOT'] . '/public/imagenes/');
 
 function incluirTemplate(string $nombre, bool $inicio = false)
 {
